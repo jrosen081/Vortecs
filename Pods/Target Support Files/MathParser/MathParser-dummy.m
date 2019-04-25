f591b39b248608a8c2ef473c6bc84719854c3018
jrosen081 <jrosen081@gmail.com>
2019-04-25 14:14:49 -0400

    Got rid of Pods from source control

--- Pods/Target Support Files/MathParser/MathParser-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MathParser : NSObject
-@end
-@implementation PodsDummy_MathParser
-@end
--- Pods/Target Support Files/Pods-Vortecs/Pods-Vortecs-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Vortecs : NSObject
-@end
-@implementation PodsDummy_Pods_Vortecs
-@end
